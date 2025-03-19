fb98486a74dec5ff2ff8ca89d9df5dba69e00f8b
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-19 15:16:24 +0100

    update macos

--- macos-sign.sh
+++ macos-sign.sh
@@ -21,6 +21,20 @@ case $i in
 esac
 done
 
+if [ -z "$INPUT_PATH" ]; then
+    echo "Input path is required"
+    exit 1
+else
+    echo "Input path: $INPUT_PATH"
+fi
+
+if [ -z "$CERT_ID" ]; then
+    echo "Certificate ID is required"
+    exit 1
+else
+    echo "Certificate ID: $CERT_ID"
+fi
+
 codesign --options "runtime" --timestamp --force --deep --sign $CERT_ID $INPUT_PATH
 
 spctl -a -vvv $INPUT_PATH
