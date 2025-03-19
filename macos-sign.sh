aedf8a63e79c4c2dd7409a66f36698371888817c
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-19 14:30:32 +0100

    macos sign

---
+++ macos-sign.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INPUT_PATH=""
+CERT_ID=""
+
+#checking for parameters
+for i in "$@"
+do
+case $i in
+    -i=*|--input_path=*)
+        INPUT_PATH="${i#*=}"
+        shift # past argument=value
+        ;;
+    -ci=*|--cert_id=*)
+        CERT_ID="${i#*=}"
+        shift # past argument=value
+        ;;
+    *)
+        # unknown option
+        ;;
+esac
+done
+
+codesign --options "runtime" --timestamp --force --deep --sign $CERT_ID $INPUT_PATH
+
+spctl -a -vvv $INPUT_PATH
