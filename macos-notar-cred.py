cff428237f8ec0a0d95ae007b343860bc3072925
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-20 11:29:28 +0100

    input path 2

---
+++ macos-notar-cred.py
@@ -0,0 +1,27 @@
+import sys
+import argparse
+import subprocess
+
+def main():
+    parser = argparse.ArgumentParser(description='Notarize files using notarytool')
+    parser.add_argument('-nu', '--notarization_user', required=True, help='Apple ID for notarization')
+    parser.add_argument('-nt', '--notarization_team', required=True, help='Team ID for notarization')
+    parser.add_argument('-np', '--notarization_pssw', required=True, help='Password for notarization')
+
+    args = parser.parse_args()
+
+    notar_user = args.notarization_user
+    notar_team_id = args.notarization_team
+    notar_password = args.notarization_pssw
+
+    try:
+        subprocess.run([
+                    'xcrun', 'notarytool', 'store-credentials', 'notarytool-profile', 
+                    '--apple-id', notar_user, '--team-id', notar_team_id, '--password', notar_password
+                ], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
--- macos-notarize.py
+++ macos-notarize.py
@@ -7,43 +7,32 @@ import glob
 def main():
     parser = argparse.ArgumentParser(description='Notarize files using notarytool')
     parser.add_argument('-i', '--input_path', required=True, help='Path to the input file or directory')
-    parser.add_argument('-nu', '--notarization_user', required=True, help='Apple ID for notarization')
-    parser.add_argument('-nt', '--notarization_team', required=True, help='Team ID for notarization')
-    parser.add_argument('-np', '--notarization_pssw', required=True, help='Password for notarization')
 
     args = parser.parse_args()
 
     input_path = args.input_path
-    notar_user = args.notarization_user
-    notar_team_id = args.notarization_team
-    notar_password = args.notarization_pssw
-
-    try:
-        subprocess.run([
-                    'xcrun', 'notarytool', 'store-credentials', 'notarytool-profile', 
-                    '--apple-id', notar_user, '--team-id', notar_team_id, '--password', notar_password
-                ], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
 
     matching_files = glob.glob(input_path)
     
     for appbundle in matching_files:
-        if appbundle.endswith('.app'):
+        if appbundle.endswith('.app') or appbundle.endswith('.dmg'):
             try:
                 print(f"Notarizing {appbundle}...")
 
-                subprocess.run(['ditto', '-c', '-k', '--keepParent', appbundle, 'notarization.zip'], check=True)
+                file_to_send = appbundle
+                if appbundle.endswith('.app'):
+                    file_to_send = 'notarization.zip'
+                    subprocess.run(['ditto', '-c', '-k', '--keepParent', appbundle, 'notarization.zip'], check=True)
 
                 subprocess.run([
-                    'xcrun', 'notarytool', 'submit', 'notarization.zip', 
+                    'xcrun', 'notarytool', 'submit', file_to_send, 
                     '--keychain-profile', 'notarytool-profile', '--wait'
                 ], check=True)
 
-                subprocess.run(['xcrun', 'stapler', 'staple', appbundle], check=True)
+                subprocess.run(['xcrun', 'stapler', 'staple', file_to_send], check=True)
 
-                os.remove('notarization.zip')
+                if os.path.exists('notarization.zip'):
+                    os.remove('notarization.zip')
 
             except subprocess.CalledProcessError as e:
                 print(f"An error occurred: {e}")
