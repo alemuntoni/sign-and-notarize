a663c43e4a106fd08ce3f84053db4114cb9b3dd5
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-20 09:32:54 +0100

    macos sign python

--- macos-sign.py
+++ macos-sign.py
@@ -4,16 +4,14 @@ import argparse
 import subprocess
 
 def main():
-    parser = argparse.ArgumentParser(description='Sign files using signtool.exe')
+    parser = argparse.ArgumentParser(description='Sign files using codesign')
     parser.add_argument('-i', '--input_path', required=True, help='Path to the input file or directory')
-    parser.add_argument('-cf', '--cert_file', required=True, help='Path to the certificate file')
-    parser.add_argument('-cp', '--cert_pssw', required=True, help='Password for the certificate file')
+    parser.add_argument('-ci', '--cert_id', required=True, help='Certificate ID')
 
     args = parser.parse_args()
 
     input_path = args.input_path
-    cert_file = args.cert_file
-    cert_pssw = args.cert_pssw
+    cert_id = args.cert_id
 
     if not os.path.exists(input_path):
         print(f"Input path {input_path} not found. Exiting...")
@@ -21,30 +19,26 @@ def main():
     else:
         print(f"Input path: {input_path}")
 
-    if not os.path.isfile(cert_file):
-        print(f"Certificate file {cert_file} not found. Exiting...")
+    if not cert_id:
+        print("Certificate ID is required. Exiting...")
         sys.exit(1)
 
-    cert_win = cert_file.replace('/', '\\')
-
     def sign_file(file_path):
-        file_win = file_path.replace('/', '\\')
         try:
             subprocess.run([
-                'signtool.exe', 'sign', '/fd', 'SHA256', '/f', cert_win, '/p', cert_pssw, 
-                '/t', 'http://timestamp.comodoca.com/authenticode', file_win
+                'codesign', '--options', 'runtime', '--timestamp', '--force', '--deep', '--sign', cert_id, file_path
             ], check=True)
+            subprocess.run(['spctl', '-a', '-vvv', file_path], check=True)
         except subprocess.CalledProcessError as e:
             print(f"An error occurred: {e}")
             sys.exit(1)
 
-    if os.path.isfile(input_path):
+    if os.path.isfile(input_path) or input_path.endswith('.app'):
         sign_file(input_path)
     elif os.path.isdir(input_path):
         for root, _, files in os.walk(input_path):
             for file in files:
-                if file.endswith('.dll') or file.endswith('.exe'):
-                    sign_file(os.path.join(root, file))
+                sign_file(os.path.join(root, file))
     else:
         print("Input path not found. Exiting...")
         sys.exit(1)
