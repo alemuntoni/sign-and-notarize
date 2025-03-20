06d5d015b657c5101992fe8f69ef954c889a03bc
alemuntoni <alemuntoni@users.noreply.github.com>
2025-03-20 10:28:30 +0100

    macos sign regexp

--- macos-sign.py
+++ macos-sign.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import subprocess
+import glob
 
 def main():
     parser = argparse.ArgumentParser(description='Sign files using codesign')
@@ -13,17 +14,22 @@ def main():
     input_path = args.input_path
     cert_id = args.cert_id
 
-    if not os.path.exists(input_path):
-        print(f"Input path {input_path} not found. Exiting...")
-        sys.exit(1)
-    else:
-        print(f"Input path: {input_path}")
-
     if not cert_id:
         print("Certificate ID is required. Exiting...")
         sys.exit(1)
 
+    # if not os.path.exists(input_path):
+    #     print(f"Input path {input_path} not found. Exiting...")
+    #     sys.exit(1)
+    # else:
+    #     print(f"Input path: {input_path}")
+
+    matching_files = glob.glob(input_path)
+
+    print(f"Matching files: {matching_files}")
+
     def sign_file(file_path):
+        print(f"Signing {file_path}...")
         try:
             subprocess.run([
                 'codesign', '--options', 'runtime', '--timestamp', '--force', '--deep', '--sign', cert_id, file_path
@@ -33,15 +39,13 @@ def main():
             print(f"An error occurred: {e}")
             sys.exit(1)
 
-    if os.path.isfile(input_path) or input_path.endswith('.app'):
-        sign_file(input_path)
-    elif os.path.isdir(input_path):
-        for root, _, files in os.walk(input_path):
-            for file in files:
-                sign_file(os.path.join(root, file))
-    else:
-        print("Input path not found. Exiting...")
-        sys.exit(1)
+    for file in matching_files:
+        if os.path.isfile(file) or file.endswith('.app'):
+            sign_file(file)
+        elif os.path.isdir(file):
+            for root, _, files in os.walk(file):
+                for f in files:
+                    sign_file(os.path.join(root, f))
 
 if __name__ == "__main__":
     main()
